e01bf1608b8ff64c74824fb3a15a1a850d0e2adf
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-11-30 22:45:40 +0000

    added function to get ipfs hash

--- tests/test_ipfs_helpers.py
+++ tests/test_ipfs_helpers.py
@@ -1,9 +1,18 @@
-from erasure.ipfs import hash_to_hex
+import pytest
+from erasure.ipfs import (
+    get_ipfs_hash,
+    hash_to_hex,
+)
 
 
-def test_ipfs_hash_to_hex():
+def test_hash_to_hex():
     "should convert a b58 ipfs hash to to hex with 0x prefix"
     hex_string = "0x227e75ab3fb8ba90fbb7addb3d30bd20c676f873e0216a767084b2073e0b7d9f"
     hash_string = "QmQfJQtxGA5MzWi5HZPyCaZiPAzNkxq8U9yApihkKsWZx2"
     assert hash_to_hex(hash_string) == hex_string
-    
+
+
+def test_ipfs_hash():
+    input_data = "Hello World!\n".encode()
+    ipfs_hash = "QmfM2r8seH2GiRaC4esTjeraXEachRt8ZsSeGaWTPLyMoG"
+    assert get_ipfs_hash(input_data) == ipfs_hash
