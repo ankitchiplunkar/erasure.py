797e97499e0c539a4df9c9aa53545422e8be0532
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-12-07 16:46:01 +0000

    removing mode from tests

--- tests/test_erasure_client.py
+++ tests/test_erasure_client.py
@@ -6,17 +6,16 @@ from erasure.session import (
 from erasure.settings import (
     ERASURE_NODE_URL,
     ERASURE_ACCOUNT_PRIVATE_KEY,
-    ERASURE_MODE,
 )
 from erasure.erasure_client import ErasureClient
 
 
+mode = "test"
 version = "v1.0.0"
 setup_logging()
 w3 = init_web3(node_url=ERASURE_NODE_URL)
 
 
-
 def test_init_erasure_client():
-    erasure_client = ErasureClient(w3, ERASURE_MODE, version)
+    erasure_client = ErasureClient(w3, mode, version)
     pass
