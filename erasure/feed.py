299c81b52dd403836188d553f125b2a0717b42d3
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-12-09 18:13:19 +0000

    generate proof hash

--- erasure/feed.py
+++ erasure/feed.py
@@ -1,6 +1,13 @@
+import json
 import logging
+import multihash
 from erasure.utils import (
     initialize_contract
+)
+from erasure.crypto import (
+    generate_key,
+    encrypt,
+    multihash_sha3_256,
 )
 
 logger = logging.info(__name__)
@@ -13,9 +20,34 @@ class Feed():
 
     def __init__(self, erasure_client, feed_address):
         self.address = feed_address
+        self.erasure_client = erasure_client
         self.contract = initialize_contract(
             w3=erasure_client.w3,
             contract_address=feed_address,
             contract_name="Feed")
         self.creator = self.contract.functions.getCreator().call()
         self.operator = self.contract.functions.getOperator().call()
+
+    def create_post(self, raw_data, key=None):
+        self.assert_client_is_connected_to_creator()
+        if key is None:
+            key = generate_key()
+        proof_hash, json_proofhash_v120 = self.generate_proof_hash(raw_data, key)
+        
+
+    def generate_proof_hash(self, raw_data, key):
+        encrypted_data = encrypt(key, raw_data)
+        key_hash = multihash_sha3_256(key)
+        data_hash = multihash_sha3_256(raw_data)
+        encrypted_data_hash = multihash_sha3_256(encrypted_data)
+        # constructing the json
+        json_proofhash_v120 = json.dumps({
+            "creator": self.creator,
+            "datahash": data_hash,
+            "keyhash": key_hash,
+            "encryptedDatahash": encrypted_data_hash,
+        })
+        return self.erasure_client.w3.sha3(text=json_proofhash_v120), json_proofhash_v120
+
+    def assert_client_is_connected_to_creator(self):
+        assert self.erasure_client.account.address == self.creator
