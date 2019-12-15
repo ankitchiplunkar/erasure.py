718f3b08c721665eeca3a57c5c028516b7108f04
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-12-15 09:33:26 +0000

    removed hard coded operator (#18)

--- erasure/contracts.py
+++ erasure/contracts.py
@@ -1,20 +1,17 @@
 CONTRACTS = {
     'mainnet': {
         "v1.2.0": {
-            "FeedFactory": "",
-            "ErasurePosts": ""
+            "FeedFactory": ""
         }
     },
     'rinkeby': {
         "v1.2.0": {
-            "FeedFactory": "0xDE19C478b2eD51668e36704b2341b81DEBFe2c40",
-            "ErasurePosts": "0x57EB544cCA126D356FFe19D732A79Db494ba09b1"
+            "FeedFactory": "0xDE19C478b2eD51668e36704b2341b81DEBFe2c40"
         }
     },
     'test': {
         "v1.2.0": {
-            "FeedFactory": "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0",
-            "ErasurePosts": "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab"
+            "FeedFactory": "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0"
         }
     }
 }
--- tests/test_erasure_client.py
+++ tests/test_erasure_client.py
@@ -1,9 +1,12 @@
-from tests.common import erasure_client
+from tests.common import (
+    erasure_client,
+    test_operator,
+)
 
 
 def test_create_feed():
-    receipt = erasure_client.create_feed()
+    receipt = erasure_client.create_feed(operator=test_operator)
     instance_created = erasure_client.feed_factory.events.InstanceCreated().processReceipt(receipt)
     assert instance_created[0]['args']['creator'] == erasure_client.account.address
     feed_initialized = erasure_client.feed_template.events.Initialized().processReceipt(receipt)
-    assert feed_initialized[0]['args']['operator'] == erasure_client.contract_dict['ErasurePosts']
+    assert feed_initialized[0]['args']['operator'] == test_operator
