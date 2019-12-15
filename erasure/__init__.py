50fe54b868f042f11fc4443ee363bf210d4e832c
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-12-15 12:42:02 +0000

    initial commit to upload libbrary to pypi

---
+++ erasure/__init__.py
@@ -0,0 +1,7 @@
+from erasure.erasure_client import ErasureClient
+from erasure.feed import Feed
+from erasure.post import Post
+from erasure.session import setup_logging
+
+
+setup_logging()
--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
+from os import path
 from setuptools import (
     find_packages,
     setup,
 )
 
-
+# managing version
 file_name = 'VERSION'
 with open(file_name, 'r') as f:
     VERSION = f.read()
+
+# managing readme
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='erasure',
     version=VERSION,
     py_modules=['erasure'],
     description="""A python library to interact with the erasure protocol.""",
-    long_description_markdown_filename='README.md',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ankit Chiplunkar',
     author_email='ankitchiplunkar@gmail.com',
     url='https://github.com/ankitchiplunkar/erasure.py',
@@ -21,8 +28,16 @@ setup(
     python_requires='>=3.6, <4',
     keywords='ethereum',
     packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[
+        "web3==5.3.0",
+        "cryptography==2.8",
+        "requests==2.22.0",
+        "pymultihash==0.8.2",
+        "pytest==5.3.1",
+        "ipfshttpclient==0.4.12",
+      ],
     classifiers=[
-        'Development Status :: Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
