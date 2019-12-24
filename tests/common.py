81e063e9d5558983573c499b9aa888b2ed0a0455
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2019-12-24 09:01:53 +0000

    replaced common test usables with pytest fixtures

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,4 @@
+import pytest
 from erasure.session import (
     init_web3,
     initialize_erasure_account
@@ -14,13 +15,21 @@ version = "v1.2.0"
 ERASURE_NODE_URL = 'http://localhost:8545'
 raw_data = bytes("multihash", "utf-8")
 key = b'B1yfUQ64D86WaumL1vjm1Ua7-7j0_YjjdOlsA-y9bQo='
-test_operator="0x0000000000000000000000000000000000000000"
+test_operator = "0x0000000000000000000000000000000000000000"
+
 # Initializing erasure client
-w3 = init_web3(node_url=ERASURE_NODE_URL)
-erasure_client = ErasureClient(w3, mode, version)
-erasure_client.update_key_store('/tmp')
+@pytest.fixture(scope="session")
+def init_erasure_client():
+    w3 = init_web3(node_url=ERASURE_NODE_URL)
+    erasure_client = ErasureClient(w3, mode, version)
+    erasure_client.update_key_store('/tmp')
+    return erasure_client
+
 # Initializing feed
-receipt = erasure_client.create_feed(operator=test_operator)
-instance_created = erasure_client.feed_factory.events.InstanceCreated().processReceipt(receipt)
-FEED_ADDRESS = instance_created[0]['args']['instance']
-feed = Feed(erasure_client=erasure_client, feed_address=FEED_ADDRESS)
+@pytest.fixture(scope="session")
+def init_feed(init_erasure_client):
+    receipt = init_erasure_client.create_feed(operator=test_operator)
+    instance_created = init_erasure_client.feed_factory.events.InstanceCreated(
+    ).processReceipt(receipt)
+    FEED_ADDRESS = instance_created[0]['args']['instance']
+    return Feed(erasure_client=init_erasure_client, feed_address=FEED_ADDRESS)
